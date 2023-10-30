a2cb14f7fe7fff16a987703d842ea8d9e117b3e1
dev-hd11 <himankdeka16@gmail.com>
2023-10-30 15:14:07 +0530

    Updating files

--- confidante/settings.py
+++ confidante/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$@&8@ptd0qb3ew@jgq!!b)=$se#t@d7^wm=o1ibwpb23(36vo8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
