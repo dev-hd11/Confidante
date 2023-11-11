6ab144c8b80e36e804ac89637414545a665720ef
dev-hd11 <himankdeka16@gmail.com>
2023-11-11 16:55:33 +0530

    v0.1.0

--- main/models.py
+++ main/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Auth(models.Model) :
-    username = models.CharField(max_length=100)
-    password = models.DecimalField(max_digits=8, decimal_places=0)
---
+++ staticfiles/homeJS.js
@@ -0,0 +1,7 @@
+function change(code) {
+    if (code == 1) {
+        document.getElementById("frst").className = "spinner-grow spinner-grow-sm";
+    } else {
+        document.getElementById("scnd").className = "spinner-grow spinner-grow-sm";
+    }
+}
