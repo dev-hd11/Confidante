1d70be9cbdc93535a0b3150b0bf6762ab3ab01ac
dev-hd11 <himankdeka16@gmail.com>
2023-11-12 13:44:58 +0530

    v0.1.1

--- staticfiles/homeJS.js
+++ staticfiles/homeJS.js
@@ -1,7 +1,33 @@
 function change(code) {
     if (code == 1) {
         document.getElementById("frst").className = "spinner-grow spinner-grow-sm";
-    } else {
+    } else if (code == 2) {
         document.getElementById("scnd").className = "spinner-grow spinner-grow-sm";
+    } else if (code == 3) {
+        document.getElementById("thrd").className = "spinner-grow spinner-grow-sm";
+    } else if (code == 4) {
+        document.getElementById("frth").className = "spinner-grow spinner-grow-sm";
+    } else if (code == 5) {
+        animateAbt();
+    } else if (code == 6) {
+        document.getElementById("six").className = "spinner-grow spinner-grow-sm";
     }
+}
+
+setInterval(getTime, 10000);
+
+function getTime() {
+    const current = new Date();
+    var str_form = String(current.getMinutes())
+    document.getElementById("time").innerText = `${(current.getHours() > 12) ? current.getHours() - 12 : current.getHours()}:${(current.getMinutes() < 10) ? "0" + str_form : current.getMinutes()}`;
+}
+
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+async function animateAbt() {
+    document.getElementById("ffth").className = "spinner-grow spinner-grow-sm";
+    await sleep(1000);
+    document.getElementById("ffth").className = "";
 }
