dd9d3614adc0655c4b1a8ce144a8d83905ddea04
dev-hd11 <himankdeka16@gmail.com>
2023-11-14 21:12:31 +0530

    v0.4.1

--- confidante/settings.py
+++ confidante/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '####'
+SECRET_KEY = 'django-insecure-3c1487a311449414c7030140150e9997ccfd83fe69cdddcfb552429bcdfcdabe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
