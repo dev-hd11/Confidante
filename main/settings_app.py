4ff1d02e8b8f4371bbb50acab9c4a321b9741331
dev-hd11 <himankdeka16@gmail.com>
2024-01-26 15:21:15 +0530

    Initializing new version

--- main/settings_app.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-
-PATH_TO_CLEANER = Path(__file__).resolve().parent.parent / "cleanup.py"
--- utilfiles/default.staff.js
+++
@@ -1,7 +0,0 @@
-/*
-(c) 2023, Himank Deka.
-*/
-function goTo() {
-    input = document.forms["srch"]["search"].value;
-    window.location.href = `http://localhost:8000/delete/${input}`;
-}
